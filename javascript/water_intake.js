b47cb239b5e0d814645a208551cf74fe0a6fd61c
Sudip Kotal <sudipkotal2003@gmail.com>
2024-01-18 18:19:27 +0530

    fix the unfilled data resolved

--- javascript/water_intake.js
+++ javascript/water_intake.js
@@ -3,12 +3,21 @@ const calcResults = () => {
     const weight = document.getElementById('weight').value;
     const exercise = document.getElementById('exercise-mins').value;
     const res = Math.round(((weight * (2 / 3)) + (12 * (exercise / 30))) / 8);
-   const resultElement = document.getElementById('result_show');
-   resultElement.innerHTML ='You should drink ' + res.toString() + ' bottle of water per day.'
+    if (isNaN(weight) || isNaN(exercise)) {
+      alert("Please enter only numeric values.");
+    }
+    else if (weight === "") {
+      alert("Please enter your weight.");
+    }
+    else if (exercise === "") {
+      alert("Please enter your exercise time. If you have not been active, please enter 5.");
+    }
+    else {
+      
+      const resultElement = document.getElementById('result_show');
+      resultElement.innerHTML ='You should drink ' + res.toString() + ' bottle of water per day.'
+    }
+
    return false;
   }
   
-// function for clear form
-  function resetForm() {
-    document.getElementById("personal_info").reset();
-  }
