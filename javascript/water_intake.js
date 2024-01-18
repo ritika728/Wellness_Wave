c055ea5dd5e90b2e0d437cca6cc61b4431d4b806
Sudip Kotal <sudipkotal2003@gmail.com>
2024-01-18 16:20:28 +0530

    Fix calculate func

--- javascript/water_intake.js
+++ javascript/water_intake.js
@@ -3,8 +3,9 @@ const calcResults = () => {
     const weight = document.getElementById('weight').value;
     const exercise = document.getElementById('exercise-mins').value;
     const res = Math.round(((weight * (2 / 3)) + (12 * (exercise / 30))) / 8);
-    document.getElementById('result').innerHTML = 'You should drink ' + res.toString() + ' cups of water per day.'
-    return false;
+   const resultElement = document.getElementById('result_show');
+   resultElement.innerHTML ='You should drink ' + res.toString() + ' bottle of water per day.'
+   return false;
   }
   
 // function for clear form
