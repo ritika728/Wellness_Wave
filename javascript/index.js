442f833368d84034ab5d6a5232d6660eabefcdc7
Diya Bhandari <diyabhandari@gmail.com>
2024-01-20 21:05:42 +0530

    remove unused pictures

--- javascript/index.js
+++ javascript/index.js
@@ -34,3 +34,21 @@ function sendMail() {
 
   }
 
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  
+    window.onscroll = function () {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+      } else {
+        scrollToTopBtn.style.display = "none";
+      }
+    };
+  
+    scrollToTopBtn.addEventListener("click", function () {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    });
+  });
+  
