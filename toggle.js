fa49d7cebddb5d08eccef829b23dba365c64edac
Sneha123-zudo <snehamishrasm9@gmail.com>
2024-01-23 21:15:41 +0530

    Created a toggle mode

---
+++ toggle.js
@@ -0,0 +1,10 @@
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+      icon.src = "sun-icon.png";
+    }
+    else {
+      icon.src = "moon-icon2.jpg";
+    }
+}
