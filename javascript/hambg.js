62a7776b72559cd1f1c71728d9eeb9a968f0fd89
SOHAN <sohanroychowdhury7@gmail.com>
2024-02-14 10:51:33 +0530

    Customized Scrollbar

--- javascript/hambg.js
+++ javascript/hambg.js
@@ -1,6 +1,6 @@
 let hamburger = document.querySelector('.ham');
 
-hamburger.addEventListener('click' , ()=> {
+hamburger.addEventListener('click', () => {
     let navi = document.querySelector('.navLinks');
     navi.classList.toggle("active");
 })
--- javascript/index.js
+++ javascript/index.js
@@ -1,54 +1,62 @@
 function sendMail() {
-    
+
 
   var params = {
-      name: document.getElementById("exampleInputName").value,
-      email: document.getElementById("exampleInputEmail2").value,
-      message: document.getElementById("exampleInputMessage").value,
-    };
+    name: document.getElementById("exampleInputName").value,
+    email: document.getElementById("exampleInputEmail2").value,
+    message: document.getElementById("exampleInputMessage").value,
+  };
 
-    const serviceID = "service_rzrxhgl";
-    const templateID = "template_o6ierwy";
+  const serviceID = "service_rzrxhgl";
+  const templateID = "template_o6ierwy";
 
-    var name = document.getElementById('exampleInputName').value;
-    var email = document.getElementById('exampleInputEmail2').value;
-    var message = document.getElementById('exampleInputMessage').value;
+  var name = document.getElementById('exampleInputName').value;
+  var email = document.getElementById('exampleInputEmail2').value;
+  var message = document.getElementById('exampleInputMessage').value;
 
-    if (name !== '' && email !== '' && message !== '') {
-      // All fields are filled, you can submit the form or perform other actions here
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("exampleInputName").value = "";
-          document.getElementById("exampleInputEmail2").value = "";
-          document.getElementById("exampleInputMessage").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
+  if (name !== '' && email !== '' && message !== '') {
+    // All fields are filled, you can submit the form or perform other actions here
+    emailjs.send(serviceID, templateID, params)
+      .then(res => {
+        document.getElementById("exampleInputName").value = "";
+        document.getElementById("exampleInputEmail2").value = "";
+        document.getElementById("exampleInputMessage").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
 
       })
-      .catch(err=>console.log(err));
+      .catch(err => console.log(err));
 
-    }else {
-      alert('Please fill in all the required fields.');
-    }
-      
-
+  } else {
+    alert('Please fill in all the required fields.');
   }
 
 
-  document.addEventListener("DOMContentLoaded", function () {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-  
-    window.onscroll = function () {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-      } else {
-        scrollToTopBtn.style.display = "none";
-      }
-    };
-  
-    scrollToTopBtn.addEventListener("click", function () {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    });
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  };
+
+  scrollToTopBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   });
-  
+});
+
+
+let progress = document.getElementById("progressbar");
+
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function () {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + "%";
+}
